80c3268f9a9f154b547baf13ac7a5fe2f7139e69
sapnab821 <116958121+sapnab821@users.noreply.github.com>
2024-09-03 16:33:32 -0400

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ${{secret.secret}}
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ${{secret.secret}})
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
