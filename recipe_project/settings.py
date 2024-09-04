2a6bba525be435f7030ab61941f02c3bf0a545c7
sapnab821 <116958121+sapnab821@users.noreply.github.com>
2024-09-04 07:20:54 -0400

    changes

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -131,8 +131,9 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/recipes')
+MEDIA_ROOT= BASE_DIR / 'media'
 
 
 
