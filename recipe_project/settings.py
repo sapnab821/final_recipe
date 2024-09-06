cc7f837c9bce660e5ea9f94f0e11b590b826d295
sapnab821 <116958121+sapnab821@users.noreply.github.com>
2024-09-06 02:59:31 -0400

    detail edits

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-8x(n9y@4gacy1^@
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['guarded-eyrie-34751-6e47dea9ec8f.herokuapp.com', 'localhost', '127.0.0.1']
 
